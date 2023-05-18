5d493519ab475c4695c986247b907b1db98d3774
ddan-ddan-ddan <ddanss@github.com>
2023-05-19 00:15:02 +0900

    usertable1

--- src/main/java/miniproject/gogo/domain/user.java
+++ src/main/java/miniproject/gogo/domain/user.java
@@ -1,4 +1,26 @@
 package miniproject.gogo.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name="user")
 public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length=20)
+    private String userid;
+
+    @Column(nullable = false, length=20)
+    private String password;
+
+    @Column(nullable = true)
+    private Long money;
 }
